4b6da41e534cf1c4f0789d20430b722f4d2cb2fb
Standa Opichal <opichals@gmail.com>
2023-05-08 17:27:23 +0200

    move JswSymList from jswrapper.h to jsvar.h

--- src/jswrapper.h
+++ src/jswrapper.h
@@ -19,36 +19,6 @@
 #include "jsvar.h"
 #include "jsdevices.h"
 
-/**
-#ifndef USE_FLASH_MEMORY
-#define PACKED_JSW_SYM PACKED_FLAGS
-#else
-// On the esp8266 we put the JswSym* structures into flash and thus must make word-sized aligned
-// reads. Telling the compiler to pack the structs defeats that, so we have to take it out.
-#define PACKED_JSW_SYM
-#endif
-#if defined(__arm64__)
-#undef PACKED_JSW_SYM
-#define PACKED_JSW_SYM __attribute__((aligned(2)))
-#endif
-
-
-/// Structure for each symbol in the list of built-in symbols
-typedef struct {
-  unsigned short strOffset;
-  unsigned short functionSpec; // JsnArgumentType
-  void (*functionPtr)(void);
-} PACKED_JSW_SYM JswSymPtr;
-// This should be a multiple of 2 in length or jswBinarySearch will need READ_FLASH_UINT16
-
-/// Information for each list of built-in symbols
-typedef struct {
-  const JswSymPtr *symbols;
-  const char *symbolChars;
-  unsigned char symbolCount;
-} PACKED_JSW_SYM JswSymList;
-*/
-
 /// Do a binary search of the symbol table list
 JsVar *jswBinarySearch(const JswSymList *symbolsPtr, JsVar *parent, const char *name);
 
